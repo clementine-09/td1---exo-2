111fd44c080e781b0765cb1026981d5fee38c812
clementine-09 <68041989+clementine-09@users.noreply.github.com>
2020-07-09 14:20:23 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+for (let index = 0; index < 2; index++) {
+    basic.showString("Bonjour")
+    basic.pause(100)
+    basic.showString("10853")
+    basic.pause(100)
+    basic.showArrow(ArrowNames.East)
+    basic.pause(100)
+    basic.showIcon(IconNames.Happy)
+}
